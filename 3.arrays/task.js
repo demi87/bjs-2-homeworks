29e2dbfef80a41d3c58331218e4b9400bb5522dc
demi87 <demi87@mail.ru>
2023-10-12 22:02:54 +0700

    add task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    return arr1.every ((value, index) => {
+        console.log (value, arr2[index]);
+    return value === arr2[index];
+    });
   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter(user => user.gender === gender);
+    if (filteredUsers.length === 0) {
+        return 0;
+    }
+    const totalAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+    const avarageAge = totalAge / filteredUsers.length;
+    return avarageAge;
   
 }
